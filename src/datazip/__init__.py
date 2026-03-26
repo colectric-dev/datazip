cf47e90368ec91fd66a74b85fd6d90f6fa5e0326
Alex <alex@colectric.com>
2026-03-25 19:02:43 -0600

    hatch and vcs versions

--- src/datazip/__init__.py
+++ src/datazip/__init__.py
@@ -1,3 +1,12 @@
 """Datazip."""
 
-__version__ = "0.1.0"
+import logging
+
+# Create a root logger for use anywhere within the package.
+logger = logging.getLogger("datazip")
+
+try:
+    from datazip._version import version as __version__
+except ImportError:
+    logger.warning("Version unknown because package is not installed.")
+    __version__ = "unknown"
